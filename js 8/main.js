0362d3410dba3870b0698ec1e4630df9635d90d1
Scara-08 <kackinbaevaaana@gmail.com>
2023-11-27 15:28:39 +0300

    first commit

--- js 8/main.js
+++ js 8/main.js
@@ -9,7 +9,10 @@ function clearDisplay() {
     displayValue = '';
     updateDisplay();
 }
-
+function koren(){
+   displayValue =  Math.sqrt(+displayValue)
+updateDisplay()
+}
 function calculate() {
     const result = new Function('return ' + displayValue)();
     if (result !== undefined && !isNaN(result)) {
@@ -24,3 +27,5 @@ function updateDisplay() {
     document.getElementById('display').value = displayValue;
 }
 
+
+console.log(Math.sqrt(16));
